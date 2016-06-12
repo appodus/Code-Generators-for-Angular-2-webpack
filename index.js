0ff25e22d07f08ff0593b3fea5d434dfe1267c75
Zhimin(Rex) YE <rexebin@gmail.com>
2016-06-12 22:12:09 +0100

    now create new folder for the generated component.

--- index.js
+++ index.js
@@ -14,22 +14,26 @@ const testsTemplate = fs.readFileSync(path.join(__dirname, './templates/componen
 // ensure a component name argument has been specified.
 const componentName = process.argv[2];
 if (!componentName) {
-    console.log('Error: No component name specified. Correct syntax is `cmpg my-component-name`');
-    process.exit(1);
+	console.log('Error: No component name specified. Correct syntax is `cmpg my-component-name`');
+	process.exit(1);
 }
 
 
 const className = kebabToPascal(componentName) + 'Component';
-let context = { componentName, className };
+let context = {componentName, className};
 
 // output the component files.
 try {
-    fs.writeFile(`${componentName}.component.ts`, mustache.render(typescriptTemplate, context));
-    fs.writeFile(`${componentName}.component.html`, mustache.render(htmlTemplate, context));
-    fs.writeFile(`${componentName}.scss`, mustache.render(sassTemplate, context));
-    fs.writeFile(`${componentName}.component.spec.ts`, mustache.render(testsTemplate, context));
+	let dir = `./${componentName}`;
+	if(!fs.exists(dir)){
+		fs.mkdir(dir);
+	}
+	fs.writeFile(`${componentName}/${componentName}.component.ts`, mustache.render(typescriptTemplate, context));
+	fs.writeFile(`${componentName}/${componentName}.component.html`, mustache.render(htmlTemplate, context));
+	fs.writeFile(`${componentName}/${componentName}.scss`, mustache.render(sassTemplate, context));
+	fs.writeFile(`${componentName}/${componentName}.component.spec.ts`, mustache.render(testsTemplate, context));
 } catch (e) {
-    console.log('An error occurred!', e);
+	console.log('An error occurred!', e);
 }
 
 console.log(`Created the ${componentName} component in the current directory.`);
@@ -38,12 +42,14 @@ console.log(`Created the ${componentName} component in the current directory.`);
  * Converts kebab-case to PascalCase.
  */
 function kebabToPascal(string) {
-    return capitalizeFirstLetter(string.replace(/-([a-z])/g, match => match[1].toUpperCase()));
+	return capitalizeFirstLetter(string.replace(/-([a-z])/g, match =>  match[1].toUpperCase())
+)
+	;
 }
 
 /**
  * Does what it says on the tin.
  */
 function capitalizeFirstLetter(string) {
-    return string.charAt(0).toUpperCase() + string.slice(1);
+	return string.charAt(0).toUpperCase() + string.slice(1);
 }
